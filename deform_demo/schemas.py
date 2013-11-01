f27d09259c7861fd45c8741a1bac414d8617d2c4
Pete Graham <petegraham1@gmail.com>
2013-11-01 17:04:08 +0000

    moving code out of views

--- deform_demo/schemas.py
+++ deform_demo/schemas.py
@@ -2,11 +2,41 @@ import colander
 import deform
 
 from .models import (
+    DBSession,
     Recipe as RecipeModel,
     Ingredient as IngredientModel,
 )
 
-class Ingredient(colander.MappingSchema):
+
+class SqlAlchemyModelMixin(object):
+
+    def set_values(self, appstruct):
+        """
+        Save the appstruct data to the DB
+        """
+        item = self.Meta.model()
+        item.set_values(appstruct)
+        DBSession.add(item)
+        DBSession.flush()
+
+        relations = self.Meta.relations
+        for relation_name, relation_model in relations.items():
+            related_items = appstruct.pop(relation_name)
+            self.save_related_items(related_items, item, relation_model)
+
+    def save_related_items(self, related_items, item, model):
+        """
+        Save the data for the related models.
+        """
+        for related_item in related_items:
+            # Note this is still recipe specific, but better than before, move this data to Meta
+            related_item['recipe_id'] = item.id
+            model_instance = model()
+            model_instance.set_values(related_item)
+            DBSession.add(model_instance)
+
+
+class Ingredient(colander.MappingSchema, SqlAlchemyModelMixin):
     # TODO: make a drop-down (can we have in-line add)
     # TODO: can we display in-line?
     name = colander.SchemaNode(colander.String())
@@ -17,7 +47,7 @@ class Ingredients(colander.SequenceSchema):
     ingredient = Ingredient()
 
 
-class Recipe(colander.MappingSchema):
+class Recipe(colander.MappingSchema, SqlAlchemyModelMixin):
     name = colander.SchemaNode(colander.String())
     description = colander.SchemaNode(
         colander.String(),
@@ -34,6 +64,7 @@ class Recipe(colander.MappingSchema):
 
     class Meta:
         model = RecipeModel
+        model_id = 'recipe_id'
         # NOTE: the dictionary key must match the MappingSchema attribute
         relations = {'ingredients': IngredientModel}
         #relationships['instructions'] = InstructionModel
--- deform_demo/views.py
+++ deform_demo/views.py
@@ -3,12 +3,6 @@ from pyramid.view import view_config
 import deform
 from deform import Form
 
-from .models import (
-    DBSession,
-    Recipe,
-    Ingredient,
-)
-
 from .schemas import (
     Recipe
 )
@@ -23,7 +17,7 @@ def form(request):
         try:
             appstruct = form.validate(request.POST.items())
             #serialized = schema.serialize(app_struct)
-            set_values(schema, appstruct)
+            schema.set_values(appstruct)
 
             # TODO: redirect to list of items here (base template to be used)
             # To do add flash
@@ -36,31 +30,3 @@ def form(request):
         render_form = form.render()
 
     return {"form": render_form}
-
-
-def set_values(schema, appstruct):
-    # TODO: should we move this function to be a method on the schema (added with a mixin)
-    model = schema.Meta.model
-    item = model()
-
-    item.set_values(appstruct)
-    DBSession.add(item)
-    DBSession.flush()
-
-    # set the related fields
-    relations = schema.Meta.relations
-    for relation_name, relation_model in relations.items():
-        related_items = appstruct.pop(relation_name)
-        save_related_items(related_items, item, relation_model)
-
-
-def save_related_items(related_items, item, model):
-    """
-    Save the data for the related models.
-    """
-    for related_item in related_items:
-        # Note this is still recipe specific, but better than before
-        related_item['recipe_id'] = item.id
-        model_instance = model()
-        model_instance.set_values(related_item)
-        DBSession.add(model_instance)
